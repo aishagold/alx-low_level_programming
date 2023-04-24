f684fa0ff50251baffe79e45e36c884c4a5ecccb
Aisha Ayoade <theaishagold@gmail.com>
2023-04-24 14:49:22 -0700

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main- a function that prints all possible combination of two digits
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int d, p;
+
+	for (d = '0'; d < '9'; d++)
+	{
+		for (p = d + 1; p <= '9'; p++)
+		{
+		putchar(d + '0');
+		putchar(p + '0');
+		if (d != 8 || p != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
