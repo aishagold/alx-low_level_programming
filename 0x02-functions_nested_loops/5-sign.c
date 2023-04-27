192efb3bcfff57feb24f1c101cede0078d7b8926
Aisha Ayoade <theaishagold@gmail.com>
2023-04-27 01:09:03 -0700

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign -  prints the sign of a number
  * @n: the number to be checked
- * Return: 1 if (n > 0), 0 if (n = 0), -1 if (n < 0)
+ * Return: 1 for psitive number, -1 for negative number, else 0
  */
 
 int print_sign(int n)
@@ -13,15 +13,15 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n < 0)
+	else if (n == 0)
 	{
-		_putchar('-');
-		return (-1);
+		_putchar('0');
+		return (0);
 	}
 	else
 	{
-		_putchar(0);
-		return (0);
+		_putchar('-');
+		return (-1);
 	}
 
 }
