3e17c1d7fa6cac5831d9675d0dc2342d4886278e
Aisha Ayoade <theaishagold@gmail.com>
2023-04-27 00:48:04 -0700

    8-24

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @c: the character to be checked
+ * Return: Absolute number
+ */
+
+int _abs(int c)
+{
+	if (c >= 0)
+		return (c);
+	else
+		return (-c);
+}
