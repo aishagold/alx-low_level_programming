01a6d02f78936d81a311307fa929b672c98f75c3
Aisha Ayoade <theaishagold@gmail.com>
2023-05-09 00:44:06 -0700

    1-memcpy

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _putchar - prints character 'c' to standard output.
+ * @c: input value
+ * Return: 1 on success
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}	
+	
