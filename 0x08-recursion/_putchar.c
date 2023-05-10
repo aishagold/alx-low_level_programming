0ec80ba52709a33b776ddf0ed846a21c7b39e4ac
Aisha Ayoade <theaishagold@gmail.com>
2023-05-10 10:56:01 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to standard output.
+ * @c: input value
+ * Return : 1 on success.
+ */
+
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
+
