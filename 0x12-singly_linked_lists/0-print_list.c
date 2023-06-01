067a1ef6c9665785710a40c81b88f23bd031a555
aishagold <theaishagold@gmail.com>
2023-06-01 05:00:01 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,8 +1,7 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * print_list -  prints all the elements of a list_t list.
+ * print_list - prints all the elements of a list_t list.
  * struct list_t - singly linked list
  * @h: pointer to struct list_t
  * Return: number of nodes in the linked list
@@ -12,7 +11,7 @@ size_t print_list(const list_t *h)
 {
 	size_t counter = 0;
 
-	while (*h)
+	while (h)
 	{
 		if (h == NULL)
 			printf("[0] (nil)");
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,14 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-/* Function Prototypes */
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node(list_t **head, const char *str);
-void free_list(list_t *head);
-int _putchar(char c);
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -24,5 +16,12 @@ typedef struct list_s
     unsigned int len;
     struct list_s *next;
 } list_t;
+
+/* Standard Libraries */
+#include <stdio.h>
+#include <stddef.h>
+
+/* Function Prototypes */
+size_t print_list(const list_t *h);
 
 #endif
